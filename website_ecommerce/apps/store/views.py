8fde8853290dcb8d229ca2f5762278328835ffcc
Icad <beatrixblaine@gmail.com>
2026-02-25 20:53:37 +0700

    product variants

--- website_ecommerce/apps/store/views.py
+++ website_ecommerce/apps/store/views.py
@@ -19,6 +19,9 @@ def search(request):
 def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
+    if product.parent:
+        return redirect('product_detail', category_slug=category_slug, slug=product.parent.slug)
+
     imagesstring = "{'thumbnail': '%s', 'image': '%s'}," % (product.thumbnail.url, product.image.url)
 
     for image in product.images.all():
@@ -40,7 +43,7 @@ def product_detail(request, category_slug, slug):
 
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    products = category.products.all()
+    products = category.products.filter(parent=None)
 
 
     context = {
