addc29ce10700bc74fbc1fc2ead443f44040704b
Icad <beatrixblaine@gmail.com>
2026-03-08 18:08:32 +0700

    PDF Receipts, being able to download PDF invoice from admin site, and send it to email

--- website_ecommerce/apps/cart/webhook.py
+++ website_ecommerce/apps/cart/webhook.py
@@ -6,8 +6,10 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 from .cart import Cart
+from apps.order.views import render_to_pdf
 
 from apps.order.models import Order
 
@@ -43,9 +45,26 @@ def webhook(request):
             product.num_available = product.num_available - item.quantity
             product.save()
 
-        html = render_to_string('order_confirmation.html', {'order': order})
-        send_mail('Order Confirmation', 'Your order is ' \
-        '', 'noreply@icadgadgets.com', ['muhammadirsyadibrahim21@gmail.com', order.email], fail_silently=False, html_message=html)
+        subject = 'Order confirmation'
+        from_email = 'noreply@icadgadgets.com'
+        to = ['muhammadirsyadibrahim21@gmail.com', order.email]
+        text_content = 'Your order is succesful!'
+        html_content = render_to_string('order_confirmation.html', {'order': order})
+
+
+        pdf = render_to_pdf('order_pdf.html', {'order': order})
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+        msg.attach_alternative(html_content, "text/html")
+
+        if pdf:
+            name = 'order_%s.pdf' % order.id
+            msg.attach(name, pdf, 'application/pdf')
+
+        msg.send()
+
+
+        #html = render_to_string('order_confirmation.html', {'order': order})
+        #send_mail('Order Confirmation', 'Your order is succesful!', 'noreply@icadgadgets.com', ['muhammadirsyadibrahim21@gmail.com', order.email], fail_silently=False, html_message=html)
 
     return HttpResponse(status=200)
     
